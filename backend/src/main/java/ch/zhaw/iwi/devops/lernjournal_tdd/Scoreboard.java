2a5bce4273049b2832addcc860ab21d91523569c
jank10 <jank10@gmx.net>
2024-04-02 23:32:21 +0200

    Code zum ersten Test geschrieben

--- backend/src/main/java/ch/zhaw/iwi/devops/lernjournal_tdd/Scoreboard.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/lernjournal_tdd/Scoreboard.java
@@ -2,4 +2,21 @@ package ch.zhaw.iwi.devops.lernjournal_tdd;
 
 public class Scoreboard {
     
+    private int scoreTeamA;
+    private int scoreTeamB;
+
+    public Scoreboard() {
+        // Initialize scores to 0
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+    }
+
+    //Methods to get the scores of the individual teams in data type int
+    public int getScoreteamA(){
+        return scoreTeamA;
+    }
+    public int getScoreteamB(){
+        return scoreTeamB;
+    }
+
 }
