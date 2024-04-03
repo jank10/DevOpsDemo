9a81e508e33343befb64431f61728929aad98c7f
jank10 <jank10@gmx.net>
2024-04-03 21:38:08 +0200

    Code für den 5. Test geschrieben

--- backend/src/main/java/ch/zhaw/iwi/devops/lernjournal_tdd/Scoreboard.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/lernjournal_tdd/Scoreboard.java
@@ -4,6 +4,8 @@ public class Scoreboard {
     
     private int scoreTeamA;
     private int scoreTeamB;
+    private String lastTeamscored;
+    private int lastScorerecorded;
 
     public Scoreboard() {
         // Initialize scores to 0
@@ -27,23 +29,44 @@ public class Scoreboard {
     // Methods to record points for Team A
     public void scoreTeamA1(){
         scoreTeamA += 1;
+        lastTeamscored = "A";
+        lastScorerecorded = 1;
     }
     public void scoreTeamA2(){
         scoreTeamA += 2;
+        lastTeamscored = "A";
+        lastScorerecorded = 2;
     }
     public void scoreTeamA3(){
         scoreTeamA += 3;
+        lastTeamscored = "A";
+        lastScorerecorded = 3;
     }
 
     // Methods to record points for Team B
      public void scoreTeamB1(){
         scoreTeamB += 1;
+        lastTeamscored = "B";
+        lastScorerecorded = 1;
     }
     public void scoreTeamB2(){
         scoreTeamB += 2;
+        lastTeamscored = "B";
+        lastScorerecorded = 2;
     }
     public void scoreTeamB3(){
         scoreTeamB += 3;
+        lastTeamscored = "B";
+        lastScorerecorded = 3;
+    }
+
+    //Method to undo the last score that has been entered independent of which team has scored
+    public void undoLastscore(){
+        if(lastTeamscored.equals("A")){
+            scoreTeamA -= lastScorerecorded;
+        } else{
+            scoreTeamB -= lastScorerecorded;
+        }
     }
 
 }
