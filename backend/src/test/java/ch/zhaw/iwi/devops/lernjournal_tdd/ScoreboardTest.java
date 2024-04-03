aae41cd623c47a16124dfb082fec13c11b8b3392
jank10 <jank10@gmx.net>
2024-04-03 19:04:49 +0200

    2. Test hinzugefügt + @BeforeEach hinzugefügt um vor jedem Test eine neue Scoreboard Klasse zu initialisieren

--- backend/src/test/java/ch/zhaw/iwi/devops/lernjournal_tdd/ScoreboardTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/lernjournal_tdd/ScoreboardTest.java
@@ -2,19 +2,31 @@ package ch.zhaw.iwi.devops.lernjournal_tdd;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ScoreboardTest {
    
     private Scoreboard scoreboard;
+
+    //Create a @BeforeEach test to initialize a new Scoreboard every time the test is run
+    @BeforeEach
+    public void initializeScoreboard(){
+        scoreboard = new Scoreboard();
+    }
     
     @Test
     // Test if the initial score is 0:0
     public void initialscoreTest(){
-        // Get the initial score
-        scoreboard = new Scoreboard();
-
         // Assert that the initial score is "0:0"
         assertEquals(0, scoreboard.getScoreteamA()+scoreboard.getScoreteamB());
+    }
+
+    @Test
+    // Test that the programm can give back the score of both teams in the format "000:000"
+    public void getFormattedscoreTest(){
+
+        // Assert that the initial score is "0:0"
+        assertEquals("000:000", scoreboard.getFormattedScore());
     }
 }
