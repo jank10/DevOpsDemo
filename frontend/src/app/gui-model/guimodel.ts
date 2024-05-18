819042f2c72eadfb20410b4d13ad53d88e5d7595
jank10 <jank10@gmx.net>
2024-05-18 20:46:19 +0200

    Kachelfarbe grün

--- frontend/src/app/gui-model/guimodel.ts
+++ frontend/src/app/gui-model/guimodel.ts
@@ -120,7 +120,7 @@ export class GuiModel {
                             "type": "newButton",
                             "name": { default: "Neues ToDo"},
                             "icon": "fa-file",
-                            "color": "blue",
+                            "color": "green",
                             "width": 2,
                             "form" : {
                                 "form" : "ToDoForm"
