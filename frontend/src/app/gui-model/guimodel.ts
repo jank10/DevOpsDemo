1b2b5506addffcee91c96b2153e167f3d7230bbf
jank10 <jank10@gmx.net>
2024-05-18 14:05:50 +0200

    Kachefarbe geändert

--- frontend/src/app/gui-model/guimodel.ts
+++ frontend/src/app/gui-model/guimodel.ts
@@ -119,7 +119,7 @@ export class GuiModel {
                             "type": "newButton",
                             "name": { default: "Neues ToDo"},
                             "icon": "fa-file",
-                            "color": "blue",
+                            "color": "green",
                             "width": 2,
                             "form" : {
                                 "form" : "ToDoForm"
